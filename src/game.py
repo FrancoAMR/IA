b7c33596d355d95a9b6b26f1688ffd4d5cd3badf
SergioCallanPerez <sergio.callan45@gmail.com>
2024-09-22 23:53:46 -0500

    Correcciones

--- src/game.py
+++ src/game.py
@@ -10,22 +10,6 @@ import random
 import json
 
 class Game:
-<<<<<<< Updated upstream
-
-    def createCards(self, typeCard):
-        attack_values=[0,4,1,3,2,1,2,6,2,5,0,4,1,3,2,1,2,6,2,5,0,4,1,3,2,1,2,6,2,5]
-        defense_values=[3,2,4,3,4,1,3,1,4,5,3,2,4,3,4,1,3,1,4,5,3,2,4,3,4,1,3,1,4,5]
-        cards= []
-        for i in range(30):
-            if(typeCard=="card"):
-                card= Card(index=i, attack_value=attack_values[i], defense_value=defense_values[i])
-            else:
-                card= OpponentCard(index=i, attack_value=attack_values[i], defense_value=defense_values[i])
-            cards.append(card)
-        return cards
-
-=======
->>>>>>> Stashed changes
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Window")
@@ -34,18 +18,6 @@ class Game:
 
         self.board = Board()
         self.endbutton = Endbutton()
-<<<<<<< Updated upstream
-        self.lp = Lp()
-        self.opponent_lp = OpponentLp()
-
-        self.cards = self.createCards("card")
-        self.opponent_cards = self.createCards("opcard")
-        
-        self.num_cards = 5
-        print(self.num_cards)
-        self.num_opponent_cards = 5
-        self.running = True
-=======
         #self.lp = Lp()
         #self.opponent_lp = OpponentLp()
         self.attack_values=[0,4,1,3,2,1,2,6,2,5,0,4,1,3,2,1,2,6,2,5,0,4,1,3,2,1,2,6,2,5]
@@ -76,30 +48,21 @@ class Game:
                 }
         elif (typeCard=="opCard"):
             print("TODO")
->>>>>>> Stashed changes
 
     def run(self):
         while self.running:
             events = pygame.event.get()
             self.events(events)
             self.render()
-<<<<<<< Updated upstream
-
-=======
             if(self.turnState==0):
                 self.change_state()
     
->>>>>>> Stashed changes
     def events(self, events):
         mouse_pos = pygame.mouse.get_pos()
         self.num_cards = handle_input(self.cards, self.num_cards, self.board, events)
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1: 
-<<<<<<< Updated upstream
-                    if self.endbutton.is_clicked(mouse_pos):
-                        self.cpu_turn()
-=======
                     if self.endbutton.is_clicked(mouse_pos) and (self.turnState==3 or self.turnState==5):
                         self.change_state()
     
@@ -155,7 +118,6 @@ class Game:
 
     def atk_decision():
         return True
->>>>>>> Stashed changes
 
     #turn cpu
 
@@ -163,27 +125,17 @@ class Game:
     def render(self):
         self.screen.fill(background_color)
         self.board.draw(self.screen)
-<<<<<<< Updated upstream
-        self.endbutton.draw(self.screen)
-        self.lp.draw(self.screen)
-        self.opponent_lp.draw(self.screen)
-=======
         self.endbutton.draw(self.screen, self.turnState)
 
->>>>>>> Stashed changes
 
         mouse_pos = pygame.mouse.get_pos()
         self.board.mouse(self.screen, mouse_pos)
 
         for i in range(self.num_cards):
-<<<<<<< Updated upstream
-            self.cards[i].draw(self.screen, self.num_cards)
-=======
             handData= json.dumps(self.hand[i])
             newData= json.loads(handData)
             newDraw= Card(index= newData["index"], attack_value=newData["attack_value"], defense_value=["defense_value"], state=0)
             newDraw.draw(self.screen, self.num_cards)
->>>>>>> Stashed changes
         
 #        for i in range(self.num_opponent_cards):
 #            self.opponent_cards[i].draw(self.screen, self.num_opponent_cards)
