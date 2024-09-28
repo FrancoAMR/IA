acdd97d34f0bee9a4809ead4af51e7dd030ab42f
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2024-09-28 18:34:41 -0500

    Changing to cardBattle phase

--- src/endbutton.py
+++ src/endbutton.py
@@ -7,6 +7,7 @@ class Endbutton:
         self.image = end_Button_Image
         self.width = end_Button_Width
         self.height = end_Button_Height
+        self.click_flag = False
 
         self.x = end_Button_X
         self.y = end_Button_Y
@@ -16,4 +17,6 @@ class Endbutton:
 
     def isClicked(self, mouse_Position):
         button_rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        return button_rect.collidepoint(mouse_Position)
+        self.click_flag = button_rect.collidepoint(mouse_Position)
+        print("Botón presionado: ", self.click_flag)
+        return self.click_flag
